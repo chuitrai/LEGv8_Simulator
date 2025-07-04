bfd34b811f3eb279364fbdea9af1593a663fa520
Chau Vo-Han <chauvo005@gmail.com>
2025-07-04 19:14:41 +0700

    new branch

--- src/main/java/com/mydomain/legv8simulator/common/Observer_demo.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.mydomain.legv8simulator.common;
-
-public interface Observer_demo {
-    void update(Object arg);
-}
--- src/main/java/com/mydomain/legv8simulator/simulator/control/ControlALU.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.mydomain.legv8simulator.simulator.control;
-
-public class ControlALU {
-    
-}
